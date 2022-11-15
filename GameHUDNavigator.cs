b57593bc8c57c139958837eab7070d3246edb208
Huras <hurast@gmail.com>
2022-11-15 17:04:31 -0300

    more HUDs

--- GameHUDNavigator.cs
+++ GameHUDNavigator.cs
@@ -7,4 +7,8 @@ using UnityEngine.UI;
 public class GameHUDNavigator : MonoBehaviour
 {
     Stack<GameHUD> HUDStack = new Stack<GameHUD>();
+
+    public void OpenHUD(GameHUD hud){
+        HUDStack.Push(hud);
+    }
 }
